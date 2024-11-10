eac91258542f35d441448e4748dd584aa8304b2e
Eskurrr <juegosescu@gmail.com>
2024-11-10 06:37:12 +0100

    devices screen

---
+++ PABLOESCUDERO/src/main/java/com/telemedicina/pabloescudero/main/controller/DevicesController.java
@@ -0,0 +1,32 @@
+package com.telemedicina.pabloescudero.main.controller;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class DevicesController {
+    @FXML
+    private MFXButton DV3;
+    @FXML
+    private MFXButton DV2;
+    @FXML
+    private MFXButton DV5;
+    @FXML
+    private MFXButton DV4;
+    @FXML
+    private MFXButton DV6;
+    @FXML
+    private ImageView userFoto;
+    @FXML
+    private Label userName;
+    @FXML
+    private GridPane DV1_Pane;
+    @FXML
+    private Label device;
+    @FXML
+    private MFXButton DV1;
+    @FXML
+    private Label DeviceName;
+}
--- PABLOESCUDERO/src/main/java/com/telemedicina/pabloescudero/main/controller/MainController.java
+++ PABLOESCUDERO/src/main/java/com/telemedicina/pabloescudero/main/controller/MainController.java
@@ -2,18 +2,38 @@ package com.telemedicina.pabloescudero.main.controller;
 
 
 import io.github.palexdev.materialfx.controls.MFXRadioButton;
+import io.github.palexdev.materialfx.controls.MFXRectangleToggleNode;
+import io.github.palexdev.materialfx.controls.MFXScrollPane;
+import io.github.palexdev.materialfx.utils.ScrollUtils;
+import io.github.palexdev.materialfx.utils.ToggleButtonsUtil;
+import io.github.palexdev.mfxcore.utils.loader.MFXLoader;
+import io.github.palexdev.mfxcore.utils.loader.MFXLoaderBean;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.controlsfx.glyphfont.FontAwesome;
 import org.controlsfx.glyphfont.Glyph;
 
+import java.util.List;
+
+import static io.github.palexdev.mfxresources.MFXResources.loadURL;
+
 
 public class MainController {
+    @FXML
+    public MFXScrollPane scrollPane;
+    @FXML
+    public VBox navBar;
+    @FXML
+    private final ToggleGroup toggleGroup;
     @FXML
     public VBox sidePanel;
     @FXML
@@ -27,7 +47,13 @@ public class MainController {
     @FXML
     public Glyph menuIcon;
 
+    private double xOffset;
+    private double yOffset;
 
+    public MainController() {
+        this.toggleGroup = new ToggleGroup();
+        ToggleButtonsUtil.addAlwaysOneSelectedSupport(toggleGroup);
+    }
     @FXML
     public void initialize() {
         menuIcon.setIcon(FontAwesome.Glyph.BARS);
@@ -35,6 +61,10 @@ public class MainController {
         mainContent.prefWidthProperty().bind(screen.widthProperty().subtract(sidePanel.widthProperty()));
         mainMenu.prefWidthProperty().bind(screen.widthProperty().subtract(sidePanel.widthProperty()));
         mainContent.prefHeightProperty().bind(screen.heightProperty().subtract(mainMenu.heightProperty()));
+
+        initializeLoader();
+
+        ScrollUtils.addSmoothScrolling(scrollPane);
     }
     @FXML
     public void toggleMenu(ActionEvent actionEvent) {
@@ -49,5 +79,46 @@ public class MainController {
     @FXML
     public void iconClicked(MouseEvent mouseEvent) {
         bt_menu.fire();
+    }
+    @FXML
+    private void initializeLoader() {
+        MFXLoader loader = new MFXLoader();
+        loader.addView(MFXLoaderBean.of("Devices", getClass().getResource("/com/telemedicina/pabloescudero/fxml/DevicesScreen.fxml"))
+                .setBeanToNodeMapper(() -> createToggle(FontAwesome.Glyph.MOBILE_PHONE, "Devices"))
+                .setDefaultRoot(true).get());
+        loader.setOnLoadedAction(beans -> {
+            List<ToggleButton> nodes = beans.stream()
+                    .map(bean -> {
+                        ToggleButton toggle = (ToggleButton) bean.getBeanToNodeMapper().get();
+                        toggle.setOnAction(event -> mainContent.getChildren().setAll(bean.getRoot()));
+                        if (bean.isDefaultView()) {
+                            mainContent.getChildren().setAll(bean.getRoot());
+                            toggle.setSelected(true);
+                        }
+                        return toggle;
+                    })
+                    .toList();
+            navBar.getChildren().setAll(nodes);
+        });
+        loader.start();
+    }
+
+    @FXML// Create toggle method with ControlsFX Glyph for the icon
+    private ToggleButton createToggle(FontAwesome.Glyph icon, String text) {
+        return createToggle(icon, text, 0);
+    }
+    @FXML
+    private ToggleButton createToggle(FontAwesome.Glyph icon, String text, double rotate) {
+        // Create a ControlsFX Glyph icon
+        Glyph glyphIcon = new Glyph("FontAwesome", icon).size(24);
+        if (rotate != 0) glyphIcon.setRotate(rotate);
+
+        // Create a ToggleButton with the icon and label
+        MFXRectangleToggleNode toggleNode = new MFXRectangleToggleNode(text, glyphIcon);
+        toggleNode.setAlignment(Pos.CENTER_LEFT);
+        toggleNode.setMaxWidth(Double.MAX_VALUE);
+        toggleNode.setToggleGroup(toggleGroup);
+
+        return toggleNode;
     }
 }
