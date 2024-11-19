a2063790c0faad168f95c8aec0cc848a9d0bfc6e
Eskurrr <juegosescu@gmail.com>
2024-11-19 12:40:07 +0100

    data classes and librarie path changed

--- RCPS/src/main/java/data/Users.java
+++ RCPS/src/main/java/data/Users.java
@@ -5,4 +5,5 @@ import java.util.List;
 
 public class Users {
     private static List<Usuario> usuarios = new ArrayList<Usuario>();
+    private static List<Usuario> LogIn = new ArrayList<Usuario>();
 }
