e4e7d23ee26d13fb73d8128fc9416214ab8f328a
Eskurrr <juegosescu@gmail.com>
2024-11-25 19:20:42 +0100

    data classes and librarie path changed

--- RCPS/src/main/java/data/Dispositivo.java
+++ RCPS/src/main/java/data/Dispositivo.java
@@ -4,27 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dispositivo {
-    private String id;
-    private String paciente;
-    private List<Integer> medidas;
-    public Dispositivo(String id) {
-        this.id = id;
-        this.medidas = new ArrayList<Integer>();
+    private char[] id = new char[8] ;
+    private char[] idPatient = new char[8] ;
+    private String date;
+    private String medida;
+    public Dispositivo(String id, String idPatient, String date, String medida) {
+        String idP = "4444" + id;
+        this.id = idP.toCharArray();
+        this.idPatient = idPatient.toCharArray();
+        this.date = date;
+        this.medida = medida;
     }
 
-    public String getId() {
+    public char[] getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(char[] id) {
         this.id = id;
     }
 
-    public List<Integer> getMedidas() {
-        return medidas;
+    public char[] getIdPatient() {
+        return idPatient;
     }
 
-    public void setMedidas(List<Integer> medidas) {
-        this.medidas = medidas;
+    public void setIdPatient(char[] idPatient) {
+        this.idPatient = idPatient;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida;
     }
 }
