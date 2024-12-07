054a88ef8ca80dfa717b782d18e209b9c869a9fd
Eskurrr <juegosescu@gmail.com>
2024-12-07 03:01:25 +0100

    search methods added and main screen layout changed

--- RCPS/src/main/java/com/telemedicina/rcps/main/data/Dispositivo.java
+++ RCPS/src/main/java/com/telemedicina/rcps/main/data/Dispositivo.java
@@ -6,15 +6,26 @@ import java.util.List;
 public class Dispositivo extends Usuario{
     private char[] idPatient = new char[8] ;
     private String date;
+    private boolean Oxygen;
     private int[] medida;
-    public Dispositivo(String id, String idPatient, String date, int[] medida) {
+    public Dispositivo(String id, String idPatient, boolean Oxy ,String date, int[] medida) {
         String idP = "4444" + id;
         super.setId(idP.toCharArray());
         this.idPatient = idPatient.toCharArray();
+        this.Oxygen = Oxy;
         this.date = date;
         this.medida = medida;
         super.setPassword("");
     }
+
+    public boolean isOxygen() {
+        return Oxygen;
+    }
+
+    public void setOxygen(boolean oxygen) {
+        Oxygen = oxygen;
+    }
+
     public char[] getIdPatient() {
         return idPatient;
     }
